a014f781d97cf3597d21776d5ab2dd8192ad0122
Lucas Choate <lchoate@ingen-networks.com>
2019-07-17 11:31:11 -0700

    Fixed CORS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,13 +27,13 @@
     methods: {
       deleteTodo(id){
         console.log('fired');
-        axios.delete(`http://jsonplaceholder.typicode.com/todos/$id`)
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/$id`)
                 .then(this.todos = this.todos.filter(todo => todo.id !== id))
                 .catch(err => console.log(err));
       },
       addTodo(newTodo){
         const { title, completed } = newTodo;
-        axios.post('http://jsonplaceholder.typicode.com/todoss',
+        axios.post('https://jsonplaceholder.typicode.com/todoss',
                 {
                   title,
                   completed
@@ -44,7 +44,7 @@
       },
     },
     created(){
-      axios.get('http://jsonplaceholder.typicode.com/todos?_limit=5')
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
               .then(res => this.todos = res.data)
               .catch(err => console.log(err));
     }
